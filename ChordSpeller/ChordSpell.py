cdfb85623b7e2d425dd805ea14c7655f272a1276
Arthur Nez <arthurnez@gmail.com>
2019-01-13 10:35:48 -0800

    Update ChordSpell.py
    Added Class Chord
    Now the program can spell triads but it is getting the 5th's wrong because it can't handle accidentals well enough.
    Need to create a class Note and replace the string literal note attributes being passed around with that class.

--- ChordSpeller/ChordSpell.py
+++ ChordSpeller/ChordSpell.py
@@ -1,9 +1,24 @@
 # 65 - 71 is the range of the scale A-G via ord()
-#
+# I think notes also needs to be a class so that we can pass accidentals around easier
+
+
+class Chord():
+    def __init__(self, r, a, q):
+        self.qualities = {"Maj": [4,3,4],
+                          "Min": [3,4,3],
+                          "Aug": [4,4,3],
+                          "Dim": [3,3,3],
+                          "Dom": [4,3,3]
+                          }
+        self.intervals = self.qualities[q]
+        self.name = r + q
+        self.root = r
+        self.third = next_third(r, a, self.intervals[0])
+        self.fifth = next_third(self.third[0],0, self.intervals[1])
 
 
 def build_chromatic_scale(start):
-    precedes_sharp = [65,67,68,70,71]
+    precedes_sharp = [65, 67, 68, 70, 71]
     h = ord(start)
     steps = 0
     scale = ''
@@ -20,7 +35,7 @@ def build_chromatic_scale(start):
     return scale
 
 
-def next_third(note,accidental, interval=4):
+def next_third(note, accidental=0, interval=4):
     # find the next letter
     n = ord(note)
     if n + 2 > 71:
@@ -31,7 +46,7 @@ def next_third(note,accidental, interval=4):
     scale = build_chromatic_scale(note)
     print(scale)
     scale_interval = scale.index(chr(t)) - scale.index(note) - accidental
-    print('scale interval {}  interval {}'.format(scale_interval,interval))
+    print('scale interval {}  interval {}'.format(scale_interval, interval))
     if scale_interval < interval:
         key = '#' * (interval - scale_interval)
     elif scale_interval > interval:
@@ -41,6 +56,8 @@ def next_third(note,accidental, interval=4):
     return chr(t)+key
 
 
+
+
 if __name__ == '__main__':
     while True:
         selection = input("enter a note").upper()
@@ -48,7 +65,7 @@ if __name__ == '__main__':
             choice = selection[0]
             if selection[1] == 'B':
                 accidental = -1
-            elif selection[1] =='#':
+            elif selection[1] == '#':
                 accidental = 1
             else:
                 accidental = 0
@@ -57,9 +74,13 @@ if __name__ == '__main__':
             accidental = 0
         if choice == 'Q':
             break
-        elif ord(choice) > 71 or ord(choice) < 65 or len(selection) > 2 or (selection[1] !='B' and selection[1] != '#'):
+        elif ord(choice) > 71 or ord(choice) < 65 or len(selection) > 2:
             continue
         else:
-            print('user input{}',format(selection))
-            print('parsed acdtnl {}'.format(accidental))
-            print(next_third(choice,accidental))
+            quality = input("what kind of chord?")
+
+            newChord = Chord(choice, accidental, quality)
+            print(newChord.name)
+            print(newChord.root)
+            print(newChord.third)
+            print(newChord.fifth)
