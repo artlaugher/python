4d011a23e97bcb01e31ffe5a0a6ae086708873b7
artlaugher <arthurnez@gmail.com>
2020-02-10 21:39:34 -0800

    shape drawing functions

--- TurtleTown/turtle_shapes.py
+++ TurtleTown/turtle_shapes.py
@@ -1,32 +1,42 @@
 # Author: Arthur Nez
 # Date: 2/2020
-# Description: Recursive function that draws a bunch of squares with Turtle
+# Description: turtle functions that draw fun stuff
 
 import turtle
 
-def draw_squares(jim, x_loc, y_loc, size, num):
+
+def draw_squares(jim, x_loc, y_loc, size):
+    """
+    recursive square drawing function with turtle
+    :param jim: turtle
+    :param x_loc: starting point x
+    :param y_loc: starting point y
+    :param size: length of side of square
+    """
     jim.pu()
     jim.goto(x_loc,y_loc)
     jim.pd()
     for i in range(0, 4):
         jim.forward(size)
         jim.rt(90)
-    if num == 0:
-        return
 
     if size > 1:
-        draw_squares(jim, x_loc + 10, y_loc - 10, size - 5, num)
-
-    else:
-        print("finished!")
-    if num >= 1:
-        draw_squares(jim, x_loc * -1, y_loc * -1, size, num -1)
+        draw_squares(jim, x_loc + 10, y_loc - 10, size - 5)
 
 
-turts = turtle.Turtle()
-
-draw_squares(turts, -250, 250, 50, 4)
-
+def polygon(turt, length, sides):
+    """
+    uses a turtle to draw a polygon with sides number of sides of length length
+    :param turt:  turtle
+    :param length: length of each side
+    :param sides: sides of a polygon
+    """
+    turt.pu()
+    turt.goto(0, 100)
+    turt.pd()
+    for s in range(0, sides):
+        turt.forward(length)
+        turt.rt(360/sides)
 
 
 turtle.mainloop()
